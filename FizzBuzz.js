12ecf586dc820494818f7d13106916f65100ecaa
Matheus Duarte <30513439+mfduar8766@users.noreply.github.com>
2018-11-05 15:52:05 -0500

    Update FizzBuzz.js

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,31 +1,14 @@
-function fizzBuzz(number) {
-
-    if (number % 3 === 0 && number % 5 === 0) {
-        return "Fzz Buzz";
-    } else if (number % 3 === 0) {
-        return "Fizz";
-    } else if (number % 5 === 0) {
-        return "Buzz";
-    } else {
-        return "";
+let fizzBuzz = () => {
+  for (let i = 1; i < 100; i++) {
+    if (i % 15 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
     }
-}
-console.log(fizzBuzz(3));
-console.log(fizzBuzz(5));
-console.log(fizzBuzz(15));
+    console.log(i);
+  }
+};
 
-function fizzBuzz2(number) {
-
-    let word = '';
-
-    if (number % 3 === 0) {
-        word = word.concat('Fizz');
-    }
-    if (number % 5 === 0) {
-        word = word.concat('Buzz');
-    }
-    return word;
-}
-console.log(fizzBuzz2(3));
-console.log(fizzBuzz2(5));
-console.log(fizzBuzz2(15));
+console.log(fizzBuzz());
